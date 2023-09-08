3ed93ab5022b4925d474a43e33e1a2ada2687c7d
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-09-08 21:25:44 +0100

    New Models

--- Laravel/app/Models/LieuDepot.php
+++ Laravel/app/Models/LieuDepot.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Lieu_depot extends Model
+class LieuDepot extends Model
 {
     use HasFactory;
 
