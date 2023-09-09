67a4f4aa7d1d048ec54ecc84ab44a0545d31b5f6
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-09-09 13:11:59 +0100

    New routes

--- Laravel/routes/api.php
+++ Laravel/routes/api.php
@@ -2,6 +2,9 @@
 use App\Http\Controllers\UserController ;
 use App\Http\Controllers\NotificationController ;
 use App\Http\Controllers\MaterielController ;
+use App\Http\Controllers\VilleController;
+use App\Http\Controllers\QuartierController;
+use App\Http\Controllers\LieuDepotController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login',  [UserController::class, 'authenticate']);
@@ -17,4 +20,9 @@ Route::post('/notification/update/{notification}' , [NotificationController::cla
 Route::get('/user/notifications' , [NotificationController::class, 'userNotifications']) ;
 Route::post('/createMateriel' , [MaterielController::class, 'create']) ;
 Route::get('/materiels' , [MaterielController::class, 'getMateriels']) ;
-
+Route::post('/ville' , [VilleController::class , 'create']);
+Route::get('/villes' , [VilleController::class , 'read']);
+Route::post('/quartier' , [QuartierController::class , 'create']);
+Route::get('/quartiers' , [QuartierController::class , 'read']);
+Route::post('/lieuDepot' , [LieuDepotController::class , 'create']);
+Route::get('/lieuDepots' , [LieuDepotController::class , 'read']);
